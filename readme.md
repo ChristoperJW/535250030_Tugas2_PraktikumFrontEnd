d172b301810d0d9ec5d3d1abfb45001dd7a11b5e
cjw <Christoper1177@gmail.com>
2026-09-12 18:44:41 +0700

    Adding another button

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 Website Link (This repo is deployed) : https://christoperjw.github.io/535250030_Tugas2_PraktikumFrontEnd/
+
+Pesan Tambahan : Di dalam html, ada 2 tombol. Namun tidak akan muncul berbarengan.
--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const button = document.getElementById('Tombol');
+const button1 = document.getElementById('TombolShow');
+const button2 = document.getElementById('TombolHide');
 const idCard = document.getElementById('card');
 
-button.addEventListener('click', function() {
-    idCard.classList.toggle('show');
+button1.addEventListener('click', function() {
+    idCard.classList.add('show');
+    TombolShow.style.display='none';
+    TombolHide.style.display='block'
+});
+
+button2.addEventListener('click', function() {
+    idCard.classList.remove('show');
+    TombolShow.style.display='block';
+    TombolHide.style.display='none';
 });
